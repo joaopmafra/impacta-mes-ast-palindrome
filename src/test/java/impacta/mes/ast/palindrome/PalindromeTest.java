3e9ad659ea811d46ebd852863c47e99974aa6239
João Paulo Mafra <joaopmafra@gmail.com>
2022-05-03 16:51:49 -0300

    refatoração do teste: instanciando PalindromeVerifier apenas uma vez (a classe é stateless)

--- src/test/java/impacta/mes/ast/palindrome/PalindromeTest.java
+++ src/test/java/impacta/mes/ast/palindrome/PalindromeTest.java
@@ -1,6 +1,5 @@
 package impacta.mes.ast.palindrome;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,21 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class PalindromeTest {
 
+    private PalindromeVerifier verifier = new PalindromeVerifier();
+
     @Test
     public void isPalindrome_nullString_isFalse() {
-        var result = new PalindromeVerifier().isPalindrome(null);
+        var result = verifier.isPalindrome(null);
         assertThat(result).isFalse();
     }
 
     @Test
     public void isPalindrome_emptyString_isTrue() {
-        var result = new PalindromeVerifier().isPalindrome("");
+        var result = verifier.isPalindrome("");
         assertThat(result).isTrue();
     }
 
     @Test
     public void isPalindrome_oneCharString_isTrue() {
-        var result = new PalindromeVerifier().isPalindrome("1");
+        var result = verifier.isPalindrome("1");
         assertThat(result).isTrue();
     }
 
