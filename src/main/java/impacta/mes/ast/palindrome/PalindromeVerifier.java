c0634734b333c4eb394cce70a96e0174768d9957
João Paulo Mafra <joaopmafra@gmail.com>
2022-05-03 16:37:34 -0300

    fazendo teste de string nula passar

--- src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
+++ src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
@@ -1,8 +1,11 @@
 package impacta.mes.ast.palindrome;
 
 public class PalindromeVerifier {
+
     public boolean isPalindrome(String string) {
-        if (string.isEmpty()) return true;
+        if (string == null) return false;
+        else if (string.isEmpty()) return true;
         return false;
     }
+
 }
