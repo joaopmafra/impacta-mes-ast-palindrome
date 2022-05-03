e9f8fe75266124b3d29d0da22b398b31f50afb1c
João Paulo Mafra <joaopmafra@gmail.com>
2022-05-03 16:20:34 -0300

    refatoração: teste de string vazia com implementação correta

--- src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
+++ src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
@@ -2,6 +2,7 @@ package impacta.mes.ast.palindrome;
 
 public class PalindromeVerifier {
     public boolean isPalindrome(String string) {
-        return true;
+        if (string.isEmpty()) return true;
+        return false;
     }
 }
