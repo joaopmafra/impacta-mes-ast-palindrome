fb50ef862fbc4f80ee8ba27ffc7dec660bc0fb46
João Paulo Mafra <joaopmafra@gmail.com>
2022-05-03 16:43:59 -0300

    teste de string com um caractere falhando

--- src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
+++ src/main/java/impacta/mes/ast/palindrome/PalindromeVerifier.java
@@ -5,6 +5,7 @@ public class PalindromeVerifier {
     public boolean isPalindrome(String string) {
         if (string == null) return false;
         else if (string.isEmpty()) return true;
+        else if (string.length() == 1) return true;
         return false;
     }
 
